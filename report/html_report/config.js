6ab0a5820263290ecc51336095f55af951a5ad06
mattc108 <matthewcamacho108@gmail.com>
2025-05-26 06:40:50 -0500

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,83 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Stopwatch_started.png",
+        "test": "..\\bitmaps_test\\20250526-063314\\Stopwatch_started.png",
+        "selector": "body",
+        "fileName": "Stopwatch_started.png",
+        "label": "Stopwatch started",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.4,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/stopwatch/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.35730997721354163,
+          "misMatchPercentage": "0.36",
+          "analysisTime": 18
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Stopwatch_before_one_circle.png",
+        "test": "..\\bitmaps_test\\20250526-063314\\Stopwatch_before_one_circle.png",
+        "selector": "body",
+        "fileName": "Stopwatch_before_one_circle.png",
+        "label": "Stopwatch before one circle",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.4,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/stopwatch/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.23816426595052084,
+          "misMatchPercentage": "0.24",
+          "analysisTime": 19
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Stopwatch_after_one_circle.png",
+        "test": "..\\bitmaps_test\\20250526-063314\\Stopwatch_after_one_circle.png",
+        "selector": "body",
+        "fileName": "Stopwatch_after_one_circle.png",
+        "label": "Stopwatch after one circle",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.4,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/stopwatch/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.21222432454427084,
+          "misMatchPercentage": "0.21",
+          "analysisTime": 18
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
